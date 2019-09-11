b4876ea76b117d86dbad9ee44903032a20896bc6
Daniel Cho <dh.cho428@gmail.com>
2019-09-10 23:45:35 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cb1 = New System.Windows.Forms.ComboBox()
+        Me.rtb1 = New System.Windows.Forms.RichTextBox()
+        Me.urlLbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cb1
+        '
+        Me.cb1.FormattingEnabled = True
+        Me.cb1.Location = New System.Drawing.Point(13, 13)
+        Me.cb1.Name = "cb1"
+        Me.cb1.Size = New System.Drawing.Size(259, 21)
+        Me.cb1.TabIndex = 0
+        '
+        'rtb1
+        '
+        Me.rtb1.Location = New System.Drawing.Point(12, 72)
+        Me.rtb1.Name = "rtb1"
+        Me.rtb1.Size = New System.Drawing.Size(260, 158)
+        Me.rtb1.TabIndex = 1
+        Me.rtb1.Text = ""
+        '
+        'urlLbl
+        '
+        Me.urlLbl.AutoSize = True
+        Me.urlLbl.Location = New System.Drawing.Point(13, 41)
+        Me.urlLbl.Name = "urlLbl"
+        Me.urlLbl.Size = New System.Drawing.Size(18, 13)
+        Me.urlLbl.TabIndex = 2
+        Me.urlLbl.Text = "url"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(510, 261)
+        Me.Controls.Add(Me.urlLbl)
+        Me.Controls.Add(Me.rtb1)
+        Me.Controls.Add(Me.cb1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "RSSUTUBE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cb1 As ComboBox
+    Friend WithEvents rtb1 As RichTextBox
+    Friend WithEvents urlLbl As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,104 @@
+﻿
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.ServiceModel.Syndication
+Imports System.Xml
+
+Public Class Form1
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        Dim url As String
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCBcRF18a7Qf58cCRy5xuWwQ"
+
+        urlLbl.Text = url
+
+
+        Dim rdr As New ConsoleApplication1.RSSReader
+        rdr.GetRSSFeeds(url, Me.cb1)
+
+    End Sub
+
+    Private Sub cb1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb1.SelectedIndexChanged
+        rtb1.Text = cb1.SelectedItem.ToString
+
+
+        'rtb1.Text = "sdfa"
+    End Sub
+End Class
+
+Public Class RSS
+    Public Property Id() As String
+        Get
+            Return m_Id
+        End Get
+        Set
+            m_Id = Value
+        End Set
+    End Property
+    Private m_Id As String
+    Public Property QuestionID() As String
+        Get
+            Return m_QuestionID
+        End Get
+        Set
+            m_QuestionID = Value
+        End Set
+    End Property
+    Private m_QuestionID As String
+    Public Property QuestionTitle() As String
+        Get
+            Return m_QuestionTitle
+        End Get
+        Set
+            m_QuestionTitle = Value
+        End Set
+    End Property
+    Private m_QuestionTitle As String
+    Public Property QuestionDescription() As String
+        Get
+            Return m_QuestionDescription
+        End Get
+        Set
+            m_QuestionDescription = Value
+        End Set
+    End Property
+    Private m_QuestionDescription As String
+    Public Property PublishDate() As DateTime
+        Get
+            Return m_PublishDate
+        End Get
+        Set
+            m_PublishDate = Value
+        End Set
+    End Property
+    Private m_PublishDate As DateTime
+End Class
+
+Namespace ConsoleApplication1
+
+    Public Class RSSReader
+        Public Sub GetRSSFeeds(ByVal myurl As String, ByVal cb As ComboBox)
+            Dim url As String = myurl
+            Dim feed As SyndicationFeed = Nothing
+
+            Using reader = XmlReader.Create(url)
+                feed = SyndicationFeed.Load(reader)
+                Dim syndicationItems = feed.Items.ToArray()
+
+                For i As Integer = 0 To syndicationItems.Length - 1
+                    Try
+                        Dim sdfa = syndicationItems(i).PublishDate.[Date] & " " & syndicationItems(i).Title.Text
+                        cb.Items.Add(sdfa)
+                    Catch ex As Exception
+                        Console.WriteLine(ex.Message)
+                    End Try
+                Next
+            End Using
+        End Sub
+    End Class
+
+End Namespace
+
